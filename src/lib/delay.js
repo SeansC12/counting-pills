d8c44bbddd54aad38b93de4b2b027f667d6ff198
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2024-09-24 20:42:19 +0800

    removed delay file

--- src/lib/delay.js
+++
@@ -1,8 +0,0 @@
-async function delay(ms) {
-  // return await for better async stack trace support in case of errors.
-  return await new Promise((resolve) =>
-    setTimeout(resolve, ms)
-  );
-}
-
-export default delay;
